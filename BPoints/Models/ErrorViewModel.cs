5db1b8ffec5d5a3a0f13f6e384bdb2f98465b005
PetterZiden <pziden@gmail.com>
2018-10-09 16:52:53 +0200

    Add files via upload
    Added skeleton off the project

---
+++ BPoints/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BPoints.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
