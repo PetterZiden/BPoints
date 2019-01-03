34a7a56025a18b30373233f760a588df24f489d2
Petter Zidén <petter.ziden@santanderconsumer.se>
2019-01-03 16:32:14 +0100

    Added more rewards

---
+++ BPoints/Models/Rewards/Reward.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace BPoints.Models.Rewards
+{
+    public class Reward
+    {
+        public Reward()
+        {
+                
+        }
+        public string RewardName { get; set; }
+        public int RewardId { get; set; }
+        public int PointCost { get; set; }
+    }
+}
